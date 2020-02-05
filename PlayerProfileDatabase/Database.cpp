99b45a019edd4563abef357fa8573dfe1d5f2033
s198061 <s198061@LAF-7E868C>
2020-02-05 08:50:48 -0600

    more changes

--- PlayerProfileDatabase/Database.cpp
+++ PlayerProfileDatabase/Database.cpp
@@ -1,12 +1,59 @@
 #include "pch.h"
 #include "Database.h"
 
+//using namespace std;
 
-Database::Database()
+void Database::TakeInput()
 {
+	while (!quit)
+	{
+		
+
+
+	}
 }
 
-
-Database::~Database()
+void Database::Add(Profile profile)
 {
+
+}
+
+void Database::Add(char playerName[50], int playerScore)
+{
+
+}
+
+void Database::Search(char input[30])
+{
+
+}
+
+void Database::BinSearch(int num)
+{
+
+}
+
+void Database::BinSearch(int num, int min, int max)
+{
+
+}
+
+void Database::Sort(Profile pro1, Profile pro2)
+{
+
+}
+
+void Database::Load()
+{
+
+}
+
+void Database::Save()
+{
+
+}
+
+void Database::Update(Profile & prof)
+{
+
 }
--- PlayerProfileDatabase/Database.h
+++ PlayerProfileDatabase/Database.h
@@ -6,12 +6,13 @@
 class Database
 {
 public:
-	Database();
-	~Database();
+	Database() {}
+	~Database() {}
 
 	void TakeInput();
 	
 private:
+	int size = 0;
 	char input[20];
 	bool quit = false;
 
@@ -19,18 +20,18 @@ private:
 	Profile profileList[];
 	
 private:
-	void Add();
-	void Add();
+	void Add(Profile profile);
+	void Add(char playerName[50], int playerScore);
 
-	void Search();
+	void Search(char input[30]);
 
-	void BinSearch();
-	void BinSearch();
+	void BinSearch(int num);
+	void BinSearch(int num, int min, int max);
 
-	void Sort();
+	void Sort(Profile pro1, Profile pro2);
 
 	void Load();
 	void Save();
-	void Update();
+	void Update(Profile& prof);
 };
 
